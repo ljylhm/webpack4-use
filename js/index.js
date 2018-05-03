69282ca42723ea082139446da9ce08834f02463b
ljy <948021695@qq.com>
2018-05-03 20:55:43 +0800

    提交数组快排算法

--- js/index.js
+++ js/index.js
@@ -1,106 +1,159 @@
 /**** 方法库 积累的常用的方法  ****/
 // 时间转天数
+var name = "Billy";
 var helper = {
-    version: "1.0.0",
-    /************************* 数据检测类 *******************************/
-    getDataType: function (obj) {
-        var _type = '',
-            _type_str = '';
+  name: "Jack",
+  version: "1.0.0",
+  /************************* 数据检测类 *******************************/
+  getDataType: function(obj) {
+    var _type = "",
+      _type_str = "";
 
-        _type = Object.prototype.toString.call(obj);
-        _type_str = _type.substring(8, _type.length - 1);
+    _type = Object.prototype.toString.call(obj);
+    _type_str = _type.substring(8, _type.length - 1);
 
-        return _type_str;
-    },
+    return _type_str;
+  },
 
-    // 检测是否为空对象
-    isEmptyObject: function (obj) {
-        for (var key in obj) {
-            return false;
-        };
-        return true
-    },
-
-    // 检测 argruments 是否存在且不为零
-    argCheck: function () {
-        if (this.isEmptyObject(arguments)) return false;
-        for (var i in arguments) {
-            if (!arguments[i] && arguments[i] != 0) {
-                console.warn('name or value can not be null');
-                return false;
-            }
-        }
-        return true;
-    },
-
-    /************************* 浏览器存储 *******************************/
-    sessionSet: function (name, value) {
-        if (!this.argCheck(name, value)) return;
-        sessionStorage.setItem(name, value);
-    },
-
-    sessionGet: function (name) {
-        if (!this.argCheck(name)) return window.sessionStorage;
-        return sessionStorage.getItem(name)
-    },
-
-    sessionClear: function (name) {
-        var sessionJson = window.sessionStorage;
-        if (!this.argCheck(name) && !this.isEmptyObject(sessionJson)) {
-            sessionJson.clear()
-        }
-        sessionJson.removeItem(name);
-    },
-
-    localStroageSet: function (name, value) {
-        if (!this.argCheck(name, value)) return;
-        localStorage.setItem(name, value);
-    },
-
-    localStroageGet: function (name) {
-        if (!this.argCheck(name)) return window.localStorage;
-        localStorage.getItem(name);
-    },
-
-    localStroageClear: function (name) {
-        var localStroageJson = window.localStroage;
-        if (!this.argCheck(name) && !this.isEmptyObject(localStroageJson)) {
-            localStroageJson.clear()
-        }
-        localStroageJson.removeItem(name);
-    },
-
-    /************************* 时间戳 *******************************/
-    // 时间戳转换为天数 时间戳皆转为毫秒
-    praseDays: function (timeStamp) {
-        var tDayStamp = 1000 * 60 * 60 * 24;
-        var tDiff = Math.floor(timeStamp / tDayStamp);
-        return tDiff;
-    },
-
-    // 获取时间戳
-    getTimeStamp: function (time) {
-        if (!time) return Math.round(new Date().getTime());
-        else {
-            if (this.getDataType(time) != 'Date') {
-                return Math.round(new Date(time).getTime());
-            }
-            return Math.round(time.getTime())
-        }
-    },
-
-    // 转换为相差的天数 向下取整
-    transformDays: function (sDate1, sDate2) {
-        sDate2 = sDate2 ? sDate2 : new Date();
-        var _stamp1 = this.getTimeStamp(sDate1),
-            _stamp2 = this.getTimeStamp(sDate2);
-        return this.praseDays(Math.abs(_stamp1 - _stamp2));
+  // 检测是否为空对象
+  isEmptyObject: function(obj) {
+    for (var key in obj) {
+      return false;
     }
+    return true;
+  },
 
-    /************************* http请求 *******************************/
+  // 检测 argruments 是否存在且不为零
+  argCheck: function() {
+    if (this.isEmptyObject(arguments)) return false;
+    for (var i in arguments) {
+      if (!arguments[i] && arguments[i] != 0) {
+        console.warn("name or value can not be null");
+        return false;
+      }
+    }
+    return true;
+  },
 
-}
+  /************************* 浏览器存储 *******************************/
+  sessionSet: function(name, value) {
+    if (!this.argCheck(name, value)) return;
+    sessionStorage.setItem(name, value);
+  },
 
-module.exports = helper;
+  sessionGet: function(name) {
+    if (!this.argCheck(name)) return window.sessionStorage;
+    return sessionStorage.getItem(name);
+  },
 
+  sessionClear: function(name) {
+    var sessionJson = window.sessionStorage;
+    if (!this.argCheck(name) && !this.isEmptyObject(sessionJson)) {
+      sessionJson.clear();
+    }
+    sessionJson.removeItem(name);
+  },
 
+  localStroageSet: function(name, value) {
+    if (!this.argCheck(name, value)) return;
+    localStorage.setItem(name, value);
+  },
+
+  localStroageGet: function(name) {
+    if (!this.argCheck(name)) return window.localStorage;
+    localStorage.getItem(name);
+  },
+
+  localStroageClear: function(name) {
+    var localStroageJson = window.localStroage;
+    if (!this.argCheck(name) && !this.isEmptyObject(localStroageJson)) {
+      localStroageJson.clear();
+    }
+    localStroageJson.removeItem(name);
+  },
+
+  /************************* 时间戳 *******************************/
+  // 时间戳转换为天数 时间戳皆转为毫秒
+  praseDays: function(timeStamp) {
+    var tDayStamp = 1000 * 60 * 60 * 24;
+    var tDiff = Math.floor(timeStamp / tDayStamp);
+    return tDiff;
+  },
+
+  // 获取时间戳
+  getTimeStamp: function(time) {
+    if (!time) return Math.round(new Date().getTime());
+    else {
+      if (this.getDataType(time) != "Date") {
+        return Math.round(new Date(time).getTime());
+      }
+      return Math.round(time.getTime());
+    }
+  },
+
+  // 转换为相差的天数 向下取整
+  transformDays: function(sDate1, sDate2) {
+    sDate2 = sDate2 ? sDate2 : new Date();
+    var _stamp1 = this.getTimeStamp(sDate1),
+      _stamp2 = this.getTimeStamp(sDate2);
+    return this.praseDays(Math.abs(_stamp1 - _stamp2));
+  },
+
+  /************************* http请求 *******************************/
+
+  /************************* 字符串方法 *******************************/
+  // 获得对应字符串的数量
+  getCharNum: function(str1, str2) {
+    if (!this.argCheck(str1, str2)) return 0;
+    var count = 0,
+      reg = new RegExp(str2, "g");
+    str1.replace(reg, function() {
+      count++;
+    });
+    return count;
+  },
+
+  /************************* 对象的方法 *******************************/
+  // 判断是否两个对象是否相等
+  isEql: function(obj1, obj2) {
+    var _arr1 = [],
+      _arr2 = [];
+    var isFlag = true;
+    if (obj1 === obj2) return true;
+    if (helper.getDataType(obj1) != "Object") return false;
+    if (Object.keys(obj1).length != Object.keys(obj2).length) return false;
+    for (var i in obj1) {
+      if (!obj2.hasOwnProperty(i)) return false;
+      if (helper.getDataType(obj1[i]) != "Object") {
+        if (obj2[i] != obj1[i]) return false;
+      } else {
+        _arr1.push(obj1[i]);
+        _arr2.push(obj2[i]);
+      }
+    }
+    _arr1.forEach(function(ele, index, arr) {
+      isFlag = isFlag && isEql(_arr1[index], _arr2[index]);
+    });
+    return isFlag;
+  },
+
+  /************************* 排序的方法 *******************************/
+  // 快速排序
+  quickSort: function(arr, left, right) {
+    if (arr.length < 2) return arr;
+    var _mid = arr.splice(0, 1)[0];
+
+    arr.forEach(function(ele, index, arr) {
+      if (ele < _mid) left.push(ele);
+      else right.push(ele);
+    });
+
+    return this.arrSort1(left, [], []).concat(
+      [_mid],
+      this.arrSort1(right, [], [])
+    );
+  }
+  // 冒泡排序
+};
+
+export default helper;
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require("path");
 module.exports = {
-    entry: "./js/index.js",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        host: "10.101.30.91",
-        publicPath: "/dist/",
-        port: "8080",
-        inline: true,
-        compress: true, // 打包的内容进行压缩
-        hot: true
-    }
-}
+  entry: "./js/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    host: "192.168.1.115",
+    publicPath: "/dist/",
+    port: "8080",
+    inline: true,
+    open:true,
+    compress: true, // 打包的内容进行压缩
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
